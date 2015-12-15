bef443bec93d522ec27a141c1b63a56fa9b77f5f
Simo-Pekka Kerkelä <simopekka1990@gmail.com>
2015-12-15 19:02:43 +0200

    fix build system

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 /* global __dirname */
+var CopyWebpackPlugin = require('copy-webpack-plugin')
+var path = require('path')
+
 module.exports = {
 	entry: "./src/js/entry.js",
 	target: "web",
@@ -12,5 +15,10 @@ module.exports = {
 			{ test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader",
 			  query: { presets: ['react', 'es2015']}}
 		]
-	}
+	},
+	plugins: [
+		new CopyWebpackPlugin([
+			{from: './src/html'}
+		])
+	]
 }
